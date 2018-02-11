ab644415e930beb1915c52092d149bb2946f7bfd
shengjm <shangguansjm@126.com>
2018-02-11 22:52:51 +0800

    Add files via upload

---
+++ resources/lang/zh/system.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'days' => [
+        'mon'   => '周一',
+        'tues'  => '周二',
+        'wed'   => '周三',
+        'thurs' => '周四',
+        'fri'   => '周五',
+        'sat'   => '周六',
+        'sun'   => '周日',
+    ],
+
+    'pireps' => [
+        'state' => [
+            'accepted'     => '已批准',
+            'pending'      => '待审批',
+            'rejected'     => '被驳回',
+            'in_progress'  => '进行中',
+            'cancelled'    => '已取消',
+            'deleted'      => '已删除',
+        ],
+    ],
+];
